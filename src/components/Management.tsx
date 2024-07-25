42f7992cb2777153f08ab033d26dbd2f548d8a68
Parth Agrawal <pvagrawal0@gmail.com>
2024-07-25 17:51:49 -0400

    added scrollbar

--- src/components/Management.tsx
+++ src/components/Management.tsx
@@ -14,11 +14,11 @@ export const Management = () => {
 
     return (
         <div className="flex flex-col h-screen items-center justify-center bg-black font-mono text-green-500 ">
-            <div className="flex flex-col min-h-[50%] min-w-[50%] items-center justify-between border-2 rounded-md border-green-500 p-4">
+            <div className="flex flex-col min-h-[50%] min-w-[50%] gap-5 items-center justify-between border-2 rounded-md border-green-500 p-4">
                 <div className="text-3xl">
                     Task Management
                 </div>
-                <div className="flex flex-col gap-2">
+                <div className="flex flex-col border border-dotted border-green-500 px-10 gap-2 h-[400px] overflow-y-auto ">
                     {tasks.map((task) => (<Task {...task} />))}
                 </div>
 
